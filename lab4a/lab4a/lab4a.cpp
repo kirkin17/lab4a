519451ee60c52c8e24e0d3f3d79913766438faf3
kirkin17 <kirkin17@users.noreply.github.com>
2019-12-25 02:27:27 +0300

    final

--- lab4a/lab4a/lab4a.cpp
+++ lab4a/lab4a/lab4a.cpp
@@ -4,64 +4,63 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "");
-	int m, l, k, c, a, b;
-	k = c = a = b = m = l = 0;
-	int arr[20];
+	int sum, l, k, c, n, b;
+	k = c = n = b = sum = l = 0;
+	int a[20];
 
 	cout << "Введите 20 чисел\n";
 	for (int i = 0; i < 20; i++) 
 	{
 		cout << i + 1 << ": ";
-		cin >> arr[i];
+		cin >> a[i];
 	}
 
 	for (int i = 1; i < 5; i++)
 	{
-		for (int j = -5 + i * 5; j < 6 + 5 * (i - 1); j++) 
+		for (int j = -5 + i * 5; j < 5 * i + 1; j++) 
 		{
-			if (arr[j] > 0) m += arr[j];
-			if (arr[j] == 0) l++;
+			if (a[j] > 0) sum += a[j];
+			if (a[j] == 0) l++;
 		}
-		if (m > k) 
+		if (sum > k) 
 		{
-			k = m;
+			k = sum;
 			b = i;
 		}
 		if (l > c) 
 		{
 			c = l;
-			a = i;
+			n = i;
 		}
-		m = l = 0;
+		sum = l = 0;
 	}
 
-	cout << "Сумма полож. максимальна в " << b << "-й пятерке. ";
+	cout << "Пятерка с максимальной суммой: " << b << endl;
 	cout << "Больше всего нулей в: ";
 
-	if (a == 1)
+	if (n == 1)
 		for (int i = 0; i < 5; i++)
-			cout << arr[i] << " ";
-	if (a == 2)
+			cout << a[i] << " ";
+	if (n == 2)
 		for (int i = 5; i < 10; i++)
-			cout << arr[i] << " ";
-	if (a == 3)
+			cout << a[i] << " ";
+	if (n == 3)
 		for (int i = 10; i < 15; i++)
-			cout << arr[i] << " ";
-	if (a == 4)
+			cout << a[i] << " ";
+	if (n == 4)
 		for (int i = 15; i < 20; i++)
-			cout << arr[i] << " ";
+			cout << a[i] << " ";
+
 
 	for (int i = 0; i < 20 - 1; i++)
 		for (int j = i + 1; j < 20; j++)
-			if (arr[j] < arr[i])
+			if (a[j] < a[i])
 			{
-				int r = arr[j];
-				arr[j] = arr[i];
-				arr[i] = r;
+				int r = a[j];
+				a[j] = a[i];
+				a[i] = r;
 			}
 	cout << "В порядке возрастания: ";
 	for (int i = 0; i < 20; i++)
-		cout << arr[i] << " ";
-
-	return 0;
+		cout << a[i] << " ";
 }
